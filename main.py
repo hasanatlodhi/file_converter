e52ec36d3e6c9e929ba125ec06cc2859ab1e6d1f
Hasanat Lodhi <55047887+hasanatlodhi@users.noreply.github.com>
2023-09-01 15:19:47 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from ppit_to_pdf import generate_pdf
 from excel_conv import generate_pdf_from_excel
 from typing import List
 from pdfconv import generate_excel_from_pdf
+import random
 import img2pdf
 app = FastAPI()
 from conversion import *
@@ -26,52 +27,59 @@ def read_root():
 
 @app.post("/docx_to_pdf/")
 async def upload_file(file: UploadFile = File(...)):
+    random_number = random.randrange(100000, 1000000)
+    random_name = str(random_number)
     upload_dir = "user_files"
     file_path = os.path.join(upload_dir, file.filename)
     if file.filename.endswith('.docx') or file.filename.endswith('.doc'):
        with open(file_path, "wb") as f:
             f.write(file.file.read())
-       convert_docx2pdf(f"user_files/{file.filename}","output.pdf")
-       return FileResponse("output.pdf", media_type="application/pdf")
+       convert_docx2pdf(f"user_files/{file.filename}",f"{random_name}.pdf")
+       return FileResponse(f"{random_name}.pdf", media_type="application/pdf")
     
     else:
         return {"error": "Unsupported file format"}
 
 @app.post("/pdf_to_docx/")
 async def upload_file(file: UploadFile = File(...)):
+    random_number = random.randrange(100000, 1000000)
+    random_name = str(random_number)
     upload_dir = "user_files"
     file_path = os.path.join(upload_dir, file.filename)
     if file.filename.endswith('.pdf'):
         with open(file_path, "wb") as f:
             f.write(file.file.read())
-        convert_pdf2docx(f"user_files/{file.filename}","new_file.docx")  
-        return FileResponse(path="new_file.docx",filename="new_file.docx",media_type="application/vnd.openxmlformats-officedocument.wordprocessingml.document")
+        convert_pdf2docx(f"user_files/{file.filename}",f"{random_name}.docx")  
+        return FileResponse(path=f"{random_name}.docx",filename=f"{random_name}.docx",media_type="application/vnd.openxmlformats-officedocument.wordprocessingml.document")
     else:
         return {"error": "Unsupported file format"}
 
 
 @app.post("/ppit_to_pdf/")
 async def upload_file(file: UploadFile = File(...)):
+    random_number = random.randrange(100000, 1000000)
+    random_name = str(random_number)
     upload_dir = "user_files"
     file_path = os.path.join(upload_dir, file.filename)
     if file.filename.endswith('.ppt') or file.filename.endswith('.pptx'):
        with open(file_path, "wb") as f:
             f.write(file.file.read())
             
-       generate_pdf(f"user_files/{file.filename}")
-       
-       return FileResponse("Ooutput.pdf", media_type="application/pdf")
+       generate_pdf(f"user_files/{file.filename}",f"{random_name}.pdf")
+       return FileResponse(f"{random_name}.pdf", media_type="application/pdf")
     else:
         return {"error": "Unsupported file format"}
 
 @app.post("/excel_to_pdf/")
 async def upload_file(file: UploadFile = File(...)):
     upload_dir = "user_files"
+    random_number = random.randrange(100000, 1000000)
+    random_name = str(random_number)
     file_path = os.path.join(upload_dir, file.filename)
     if file.filename.endswith('.xlsx') or file.filename.endswith('.csv'):
         with open(file_path, "wb") as f:
             f.write(file.file.read())
-        generate_pdf_from_excel(f"user_files/{file.filename}")
+        generate_pdf_from_excel(f"user_files/{file.filename}",f"{random_name}.pdf")
         return FileResponse("Ooutput.pdf", media_type="application/pdf")
     else:
         return {"error": "Unsupported file format"}
@@ -79,27 +87,30 @@ async def upload_file(file: UploadFile = File(...)):
 
 @app.post("/pdf_to_excel/")
 async def upload_file(file: UploadFile = File(...)):
+    random_number = random.randrange(100000, 1000000)
+    random_name = str(random_number)
     upload_dir = "user_files"
     file_path = os.path.join(upload_dir, file.filename)
     if file.filename.endswith('.pdf'):
         with open(file_path, "wb") as f:
             f.write(file.file.read())
-        generate_excel_from_pdf(f"user_files/{file.filename}")
-        return FileResponse("output.xlsx", media_type="application/pdf")
+        generate_excel_from_pdf(f"user_files/{file.filename}",f"{random_name}.xlsx")
+        return FileResponse(f"{random_name}.xlsx",media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", filename=f"{random_name}.xlsx")
     else:
         return {"error": "Unsupported file format"}
 
 @app.post("/imgs_to_pdf/")
 async def upload_file(images: List[UploadFile] = File(...)):
-    print(images)
+    random_number = random.randrange(100000, 1000000)
+    random_name = str(random_number)
     try:
         uploaded_images = []
         for img in images:
             with open(f"user_files/{img.filename}", "wb") as f:
                 f.write(img.file.read())
                 uploaded_images.append(f"user_files/{img.filename}")
-        with open("output.pdf", "wb") as pdf_file:
+        with open(f"{random_name}.pdf", "wb") as pdf_file:
             pdf_file.write(img2pdf.convert(uploaded_images))
-        return FileResponse("output.xlsx", media_type="application/pdf")
+        return FileResponse(f"{random_name}.pdf", media_type="application/pdf")
     except:
         return {"error": "Unsupported file format"}

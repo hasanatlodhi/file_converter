7f436ad1be2f283a04a9695eef5dbb56fa050441
Hasanat Lodhi <55047887+hasanatlodhi@users.noreply.github.com>
2023-09-02 11:15:51 +0500

    Add files via upload

---
+++ pdfconv.py
@@ -0,0 +1,5 @@
+import tabula
+def generate_excel_from_pdf(input_pdf,output_excel):
+    df = tabula.read_pdf(input_pdf, pages='all')[0]
+    tabula.convert_into(input_pdf, output_excel, output_format="csv", pages='all')
+

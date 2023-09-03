5deff3cdb12fabaddb4cdd091e8e62985b6d7921
Hasanat Lodhi <55047887+hasanatlodhi@users.noreply.github.com>
2023-09-03 11:17:31 +0500

    Delete pdfconv.py

--- pdfconv.py
+++
@@ -1,5 +0,0 @@
-import tabula
-def generate_excel_from_pdf(input_pdf,output_excel):
-    df = tabula.read_pdf(input_pdf, pages='all')[0]
-    tabula.convert_into(input_pdf, output_excel, output_format="csv", pages='all')
-

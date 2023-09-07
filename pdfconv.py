d157c0c1fd86453a1c6e3a3ba231c71f7e748f6c
Hasanat Lodhi <55047887+hasanatlodhi@users.noreply.github.com>
2023-09-07 14:27:43 +0500

    Update pdfconv.py

--- pdfconv.py
+++ pdfconv.py
@@ -1,6 +1,6 @@
 import tabula
 def generate_excel_from_pdf(input_pdf,output_excel):
-    df = tabula.read_pdf(input_pdf, pages='1')
+    df = tabula.read_pdf(input_pdf, pages=1)
     tabula.convert_into(input_pdf, output_excel, output_format="csv", pages='all')
     
 

db6e46953aeb481074c5a455ddf09b4dd68569cd
Hasanat Lodhi <55047887+hasanatlodhi@users.noreply.github.com>
2023-09-07 14:47:11 +0500

    Update pdfconv.py

--- pdfconv.py
+++ pdfconv.py
@@ -1,5 +1,17 @@
 import tabula
+import aspose.pdf as pdf
 def generate_excel_from_pdf(input_pdf,output_excel):
+    # Load input PDF document
+    document = pdf.Document(input_pdf)
+    
+    # Initialize the ExcelSaveOptions
+    excelSaveOptions = pdf.ExcelSaveOptions()
+    
+    # Convert the PDF to Excel workbook
+    document.save("scccs.xlsx", excelSaveOptions)
+    
+    print("Conversion process completed")
+
     df = tabula.read_pdf(input_pdf, pages=1)
     tabula.convert_into(input_pdf, output_excel, output_format="csv", pages='all')
     
